f3b2753fa44f6cf8fb9220e6afe9b2837c990394
Lukasz Dziedziul <l.dziedziul@gmail.com>
2019-10-11 09:05:52 +0200

    5_instantiate_configurations

--- src/main/java/pl/dziedziul/myowndicontainer/engine/ContextFactory.java
+++ src/main/java/pl/dziedziul/myowndicontainer/engine/ContextFactory.java
@@ -4,6 +4,16 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static java.util.stream.Collectors.toUnmodifiableSet;
+
 public class ContextFactory {
     private static final Logger log = LoggerFactory.getLogger(ContextFactory.class);
     private final Reflections reflections;
@@ -15,6 +25,23 @@ public class ContextFactory {
     public Context createContext() {
         var configurationClasses = reflections.getTypesAnnotatedWith(Configuration.class);
         log.info("Creating context from configurations: {}", configurationClasses);
+        Set<Object> configurations = createConfigurations(configurationClasses);
         return new Context();
     }
+
+    private Set<Object> createConfigurations(final Set<Class<?>> configurationClasses) {
+        return configurationClasses.stream().map(this::createConfigurationInstance)
+                .collect(toUnmodifiableSet());
+    }
+
+    private Object createConfigurationInstance(final Class<?> configurationClass) {
+        try {
+            Constructor<?> defaultConstructor = configurationClass.getDeclaredConstructor();
+            defaultConstructor.setAccessible(true);
+            return defaultConstructor.newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalArgumentException("Configuration " + configurationClass.getSimpleName() + " must have parameterless constructor", e);
+        }
+    }
+
 }
